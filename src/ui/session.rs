acec9c554d4eca4b2f701cf9e14957781a96e67b
Trevor Strieber <trevor@strieber.org>
2022-03-28 19:08:21 -0700

    improve session history, it works!

--- src/ui/session.rs
+++ src/ui/session.rs
@@ -32,48 +32,62 @@ impl From<&str> for Page {
 #[derive(Debug, Clone)]
 pub struct SessionHistory {
     pages: Vec<Page>,
-    index: usize,
+    current: usize,
 }
 
+// TODO: is there a way to ensure that current + pages are always in sync (no bad states)
 impl SessionHistory {
     pub fn new() -> Self {
         Self {
             pages: vec![],
-            index: 0,
+            current: 0,
         }
     }
 
     pub fn go_back(&mut self) -> Option<&Page> {
-        if self.pages.len() <= 1 {
+        if !self.can_go_backward() {
             return None;
         }
 
-        if self.index < 1 {
-            return None;
-        }
+        self.current -= 1;
 
-        self.index -= 1;
-
-        Some(&self.pages[self.index])
+        Some(&self.pages[self.current])
     }
 
     pub fn go_forward(&mut self) -> Option<&Page> {
-        if self.pages.len() <= 1 {
+        if !self.can_go_forward() {
             return None;
         }
 
-        if self.index >= self.pages.len() - 1 {
-            return None;
-        }
+        self.current += 1;
 
-        self.index += 1;
-
-        Some(&self.pages[self.index])
+        Some(&self.pages[self.current])
     }
 
-    pub fn navigate(&mut self, url: &str) {
-        self.pages.push(url.into());
+    pub fn can_go_forward(&self) -> bool {
+        // not on the last element & the history needs to have more than 1 url in it
+        self.pages.len() > 1 && self.current < self.pages.len() - 1
+    }
 
-        self.index = self.pages.len() - 1;
+    pub fn can_go_backward(&self) -> bool {
+        // not on the first element & the history
+        self.pages.len() > 1 && self.current > 0
+    }
+
+    pub fn navigate(&mut self, new_url: &str) {
+        if self.can_go_forward() {
+            let page = &self.pages[self.current + 1];
+
+            if new_url == page.url() {
+                self.current += 1;
+
+                return;
+            }
+        }
+
+        self.pages.truncate(self.current + 1);
+        self.pages.push(new_url.into());
+
+        self.current = self.pages.len() - 1;
     }
 }
